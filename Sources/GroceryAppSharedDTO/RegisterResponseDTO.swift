fce1520cb4916be807651ccdeff8d0318c95cff1
simontopliss <simontopliss@me.com>
2023-06-04 20:33:34 +0100

    Add public init

--- Sources/GroceryAppSharedDTO/RegisterResponseDTO.swift
+++ Sources/GroceryAppSharedDTO/RegisterResponseDTO.swift
@@ -10,4 +10,9 @@ import Foundation
 public struct RegisterResponseDTO: Codable {
     public let error: Bool
     public var reason: String? = nil
+
+    public init(error: Bool, reason: String? = nil) {
+        self.error = error
+        self.reason = reason
+    }
 }
